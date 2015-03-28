b036819779c8c32f20f09f9cd30d4e1cba63a5bc
Logan Rickert <LoganRickert@users.noreply.github.com>
2015-03-27 23:59:41 -0400

    Returns just tweets
    Works except it has tags and converted HTML.

--- get_tweet.py
+++ get_tweet.py
@@ -1,23 +1,28 @@
+
 # Python 3
 
 import subprocess
 import requests
+from html.parser import HTMLParser
 
 def main():
 	# Get query to search
     query = get_query()
     string = get_html("https://twitter.com/" + query).split()
+
     for n in range(0, len(string)):
         if string[n] == 'dir="ltr"':
             if 'data-aria-label-part="0"' in string[n + 1]:
+                returnString = ""
                 temp = string[n + 1].split(">")
-                print(temp[1] + " ", end="")
+                returnString = returnString + temp[1] + " "
                 tempVar = n + 2
                 while "</p>" not in string[tempVar]:
-                    print(string[tempVar] + " ", end="")
+                    returnString = returnString + string[tempVar] + " "
                     tempVar += 1
-                temp = string[tempVar].split("<")
-                print(temp[0])
+                temp = string[tempVar].split("</p>")
+                returnString = returnString + temp[0]
+                print(strip_tags(returnString))
                 print("\n----------\n")
                 n = tempVar
 
@@ -27,10 +32,13 @@ def get_query():
 	query = input("Whose page would you like to get? ")
 	return query
 
+def strip_tags(html):
+    # Need to find something that does this correctly. I will most likely
+    # Have to write one myself.
+    return html
+
 def get_html(url):
-
-    url.replace(" ", "-")
-
+    """ Gets the HTML for a page. """
     print("Attempting to grab html...\n")
 
     r = requests.get(url)
