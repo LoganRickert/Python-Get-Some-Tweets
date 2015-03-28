dc4227bb64e8e6e24dff3ae3adf5f9450e9b0ac1
Logan Rickert <LoganRickert@users.noreply.github.com>
2015-03-27 23:29:25 -0400

    Code now gets tweets
    Code now gets the tweets but HTML code is still in it.

--- get_tweet.py
+++ get_tweet.py
@@ -7,8 +7,20 @@ def main():
 	# Get query to search
     query = get_query()
     string = get_html("https://twitter.com/" + query).split()
-    for word in string:
-        print(word)
+    for n in range(0, len(string)):
+        if string[n] == 'dir="ltr"':
+            if 'data-aria-label-part="0"' in string[n + 1]:
+                temp = string[n + 1].split(">")
+                print(temp[1] + " ", end="")
+                tempVar = n + 2
+                while "</p>" not in string[tempVar]:
+                    print(string[tempVar] + " ", end="")
+                    tempVar += 1
+                temp = string[tempVar].split("<")
+                print(temp[0])
+                print("\n----------\n")
+                n = tempVar
+
 
 def get_query():
 	""" Gets the page of tweets from user. """
@@ -19,7 +31,7 @@ def get_html(url):
 
     url.replace(" ", "-")
 
-    print("Attempting to grab html...")
+    print("Attempting to grab html...\n")
 
     r = requests.get(url)
 
